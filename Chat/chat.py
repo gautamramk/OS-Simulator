ea3ba5a5067c28c442d5afb5e0a402d6d9286439
Gautam Ramakrishnan <gautamrk@hotmail.com>
2018-03-27 14:47:12 +0530

    Chat working

--- Chat/chat.py
+++ Chat/chat.py
@@ -5,24 +5,25 @@ import websockets
 ws_to_client = dict()
 
 
-def handler(ws, path):
+async def handler(ws, path):
     ra = ws.remote_address
-    name = ws.recv()
+    name = await ws.recv()
     ws_to_client[ws] = name
 
     listen_task = asyncio.ensure_future(ws.recv())
     while True:
-        done, pending = yield from asyncio.wait([listen_task],return_when=asyncio.FIRST_COMPLETED)
+        done, pending = await asyncio.wait([listen_task],return_when=asyncio.FIRST_COMPLETED)
         if listen_task in done:
             message = listen_task.result()
             if message is not None:
                 sender = ws_to_client[ws]
-                tosend = sender+": "+message
+                tosend = sender+": "+str(message)
                 for key in ws_to_client:
-                    print(key)
-                    key.send(tosend)
+                    await key.send(tosend)
+
+                listen_task = asyncio.ensure_future(ws.recv())
 
 
-start_server = websockets.serve(handler, 'localhost', 8800)
+start_server = websockets.serve(handler, 'localhost', 8888)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
